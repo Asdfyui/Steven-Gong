379392759936294eb38d3c8dc2f5d942abc7ca92
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-02-22 17:23:21 -0800

    String Practice 2

---
+++ Variables/src/VariablesAndNames.java
@@ -0,0 +1,16 @@
+public class VariablesAndNames
+{
+    public static void main( String[] args )
+    {
+     int room = 113;
+     double number = 2.71828;
+     String Name = "Computer";
+     String LastName = " Science";
+
+     System.out.println("This is room #"+room);
+     System.out.println("e is close to "+number);
+     System.out.println("I am learning a bit about "+Name+LastName);
+    }
+}
+
+
