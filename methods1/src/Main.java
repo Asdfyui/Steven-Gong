25c1bc30fe4e7874e3c64b20cbb5e238928c1b90
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-03-10 14:35:19 -0800

    Array Method Assignment

---
+++ methods1/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;//addon to array
+
+public class Main {
+    public static int[] addOn(int[] array, int number) {
+        int[] newarray = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++){
+            newarray[i] = array[i];
+            newarray[array.length] = number;
+        }
+            return newarray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[9];
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (Math.random() * 100 + 1);
+            array[i] = random;
+            System.out.println(array[i]);
+        }
+        System.out.println("****");
+        System.out.println(Arrays.toString(addOn(array, 656)));
+    }
+}
+
+
+
+
---
+++ methods1/src/Main2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;// delete value in array
+
+public class Main2 {
+    public static int[] remove(int[] array) {
+        int[] newarray = new int[array.length - 1 ];
+        for (int i = 0; i < array.length - 1; i++){
+            newarray[i] = array[i];
+        }
+        return newarray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (Math.random() * 100 + 1);
+            array[i] = random;
+            System.out.println(array[i]);
+        }
+
+        System.out.println("****");
+        System.out.println("Deleted value is " + array[array.length - 1]);
+        System.out.println(Arrays.toString(remove(array)));
+    }
+}
